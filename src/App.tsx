4ed1b3b3bb3e790724fa32d48a94120835e6cb85
Sadman Sakib <ssakib4040@gmail.com>
2023-12-03 20:11:28 +0600

    Sidebar with mock items added

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,55 @@ export default function App() {
     <div>
       <Header />
 
-      <div className="flex flex-col lg:flex-row">
-        <div className="lg:basis-6/12">
-          <Editor
-            height="100vh"
-            theme="vs-dark"
-            defaultLanguage="json"
-            onChange={(value) => {
-              console.log(value);
-              setCode(value ?? "");
-            }}
-            value={code}
-            // defaultValue="// some comment"
-          />
+      <div className="flex ">
+        <div className="basis-[300px]">
+          <div className="px-3 py-2 m-2 rounded-lg cursor-pointer transition hover:bg-zinc-200">
+            test()
+          </div>
+
+          <div className="px-3 py-2 m-2 rounded-lg cursor-pointer transition hover:bg-zinc-200">
+            test()
+          </div>
+
+          <div className="px-3 py-2 m-2 rounded-lg cursor-pointer transition hover:bg-zinc-200">
+            test()
+          </div>
+
+          <div className="px-3 py-2 m-2 rounded-lg cursor-pointer transition hover:bg-zinc-200">
+            test()
+          </div>
+
+          <div className="px-3 py-2 m-2 rounded-lg cursor-pointer transition hover:bg-zinc-200">
+            test()
+          </div>
         </div>
 
-        <div className="lg:basis-6/12">
-          <Editor
-            height="100vh"
-            theme="vs-dark"
-            defaultLanguage="json"
-            value={code}
-            // defaultValue="// some comment"
-          />
+        <div className="basis-full">
+          <div className="flex flex-col lg:flex-row">
+            <div className="lg:basis-6/12">
+              <Editor
+                height="100vh"
+                theme="vs-dark"
+                defaultLanguage="json"
+                onChange={(value) => {
+                  console.log(value);
+                  setCode(value ?? "");
+                }}
+                value={code}
+                // defaultValue="// some comment"
+              />
+            </div>
+
+            <div className="lg:basis-6/12">
+              <Editor
+                height="100vh"
+                theme="vs-dark"
+                defaultLanguage="json"
+                value={code}
+                // defaultValue="// some comment"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>

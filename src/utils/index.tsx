78b1611ed363b73253509fe20829c126a0b28d5b
Sadman Sakib <ssakib4040@gmail.com>
2024-01-10 00:30:08 +0600

    Temporarily some function added successfully

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,12 +1,36 @@
-function test1() {
-  return "this is a test1";
+function generateID() {
+  return Math.random().toString(36).substr(2, 9);
 }
 
-function test2() {
-  return  "this is a test2";
+function generateNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+function generateBoolean() {
+  return Math.random() < 0.5;
+}
+
+function generateString() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const charactersLength = characters.length;
+
+  let result = "";
+
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+function generateRandomDate() {
+  return new Date().toISOString();
 }
 
 export default {
-  test1,
-  test2,
+  generateID,
+  generateNumber,
+  generateBoolean,
+  generateString,
+  generateRandomDate,
 };

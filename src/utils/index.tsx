b6c514ff3cb2d80807dbc1985e9cab96b4189b94
Sadman Sakib <ssakib4040@gmail.com>
2024-01-14 00:57:54 +0600

    Some more apis added in utils

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -53,6 +53,105 @@ function username() {
   return faker.internet.userName();
 }
 
+function imei() {
+  return faker.phone.imei();
+}
+
+function color() {
+  return faker.color.rgb();
+}
+
+function productName() {
+  return faker.commerce.productName();
+}
+
+function product() {
+  return faker.commerce.product();
+}
+
+function productPrice() {
+  return faker.commerce.price();
+}
+
+function companyName() {
+  return faker.company.name();
+}
+
+function date() {
+  return faker.date.anytime();
+}
+
+function bitcoinAddress() {
+  return faker.finance.bitcoinAddress();
+}
+
+function creditCard() {
+  return faker.finance.creditCardNumber();
+}
+
+function ethereumAddress() {
+  return faker.finance.ethereumAddress();
+}
+function pin() {
+  return faker.finance.account();
+}
+
+function imageUrl() {
+  return faker.image.url();
+}
+
+function imageBase64() {
+  return faker.image.dataUri();
+}
+
+function emoji() {
+  return faker.internet.emoji();
+}
+
+function ip() {
+  return faker.internet.ip();
+}
+
+function ipv4() {
+  return faker.internet.ipv4();
+}
+
+function ipv6() {
+  return faker.internet.ipv6();
+}
+
+function mac() {
+  return faker.internet.mac();
+}
+
+function city() {
+  return faker.location.city();
+}
+
+function country() {
+  return faker.location.country();
+}
+
+function countryCode() {
+  return faker.location.countryCode();
+}
+
+function street() {
+  return faker.location.street();
+}
+
+function streetAddress() {
+  return faker.location.streetAddress();
+}
+
+function timeZone() {
+  return faker.location.timeZone();
+}
+
+function zipCode() {
+  return faker.location.zipCode();
+}
+
 export default {
   uuid,
   fullName,
@@ -66,4 +165,29 @@ export default {
   profileImage,
   password,
   username,
+  imei,
+  color,
+  productName,
+  product,
+  productPrice,
+  companyName,
+  date,
+  bitcoinAddress,
+  creditCard,
+  ethereumAddress,
+  pin,
+  imageUrl,
+  imageBase64,
+  emoji,
+  ip,
+  ipv4,
+  ipv6,
+  mac,
+  city,
+  country,
+  countryCode,
+  street,
+  streetAddress,
+  timeZone,
+  zipCode,
 };

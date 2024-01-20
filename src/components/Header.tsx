91c43cff9e3436255081592b9d6af4be6eff431d
Sadman Sakib <ssakib4040@gmail.com>
2024-01-20 21:24:47 +0600

    Auto complete dropdown structure redefined.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,42 @@
 import { Fragment, useEffect, useState, useRef } from "react";
 import { Combobox, Transition } from "@headlessui/react";
-import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 // import utils from "../utils";
 
-interface peopleInterface{
+interface peopleInterface {
   id: number;
   name: string;
 }
 
 const people: peopleInterface[] = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
+  { id: 1, name: "Select API functions" },
+  { id: 2, name: "Select API functions" },
 ];
+
+// const apiList = [
+//   {
+//     name: "User",
+//     functions: [
+//       "id",
+//       "name",
+//       "email",
+//       "username",
+//       "phone",
+//       "image",
+//       "address",
+//       "website",
+//       "company",
+//     ],
+//   },
+//   {
+//     name: "Minecraft",
+//     functions: [""],
+//   },
+// ];
 
 export default function Header() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [selected, setSelected] = useState();
+  const [selected, setSelected] = useState(people[0]);
   const [query, setQuery] = useState("");
 
   const filteredPeople =
@@ -62,7 +79,7 @@ export default function Header() {
                     />
                     <Combobox.Button
                       className="absolute inset-y-0 right-0 flex items-center pr-2"
-                      // @ts-expect-error this is a valid ref 
+                      // @ts-expect-error this is a valid ref
                       ref={inputRef}
                     >
                       <ChevronUpDownIcon
@@ -91,13 +108,13 @@ export default function Header() {
                             className={({ active }) =>
                               `relative cursor-default select-none py-2 pl-10 pr-4 ${
                                 active
-                                  ? "bg-teal-600 text-white"
+                                  ? "bg-gray-900 text-white"
                                   : "text-gray-900"
                               }`
                             }
                             value={person}
                           >
-                            {({ selected, active }) => (
+                            {({ selected }) => (
                               <>
                                 <span
                                   className={`block truncate ${
@@ -106,18 +123,6 @@ export default function Header() {
                                 >
                                   {person.name}
                                 </span>
-                                {selected ? (
-                                  <span
-                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                      active ? "text-white" : "text-teal-600"
-                                    }`}
-                                  >
-                                    <CheckIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                ) : null}
                               </>
                             )}
                           </Combobox.Option>

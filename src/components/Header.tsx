0dc56059cb6cfe41bd729bf79f0b922055b29bc4
Sadman Sakib <ssakib4040@gmail.com>
2024-01-21 01:34:15 +0600

    Filter algorithm added in header autocomplete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,51 +3,75 @@ import { Combobox, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 // import utils from "../utils";
 
-interface peopleInterface {
-  id: number;
+// interface peopleInterface {
+//   id: number;
+//   name: string;
+// }
+
+// const people: peopleInterface[] = [
+//   { id: 1, name: "Select API functions" },
+//   { id: 2, name: "Select API functions" },
+// ];
+
+interface apiInterface {
   name: string;
+  functions: string[];
 }
 
-const people: peopleInterface[] = [
-  { id: 1, name: "Select API functions" },
-  { id: 2, name: "Select API functions" },
+const apiList: apiInterface[] = [
+  {
+    name: "User",
+    functions: [
+      "id()",
+      "name()",
+      "email()",
+      "username()",
+      "phone()",
+      "image()",
+      "address()",
+      "website()",
+      "company()",
+    ],
+  },
+  {
+    name: "Post",
+    functions: [
+      "title()",
+      "body()",
+      "userId()",
+      "comments()",
+      "createdAt()",
+      "updatedAt()",
+    ],
+  },
 ];
-
-// const apiList = [
-//   {
-//     name: "User",
-//     functions: [
-//       "id",
-//       "name",
-//       "email",
-//       "username",
-//       "phone",
-//       "image",
-//       "address",
-//       "website",
-//       "company",
-//     ],
-//   },
-//   {
-//     name: "Minecraft",
-//     functions: [""],
-//   },
-// ];
 
 export default function Header() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [selected, setSelected] = useState(people[0]);
   const [query, setQuery] = useState("");
 
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) =>
-          person.name
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+  // const filteredPeople: apiInterface[] =
+  //   query === ""
+  //     ? people
+  //     : people.filter((person) =>
+  //         person.name
+  //           .toLowerCase()
+  //           .replace(/\s+/g, "")
+  //           .includes(query.toLowerCase().replace(/\s+/g, ""))
+  // );
+
+  const filteredApiList: apiInterface[] = apiList
+    .filter((api) =>
+      api.functions.some((func) =>
+        func.toLowerCase().includes(query.toLowerCase())
+      )
+    )
+    .map((api) => ({
+      name: api.name,
+      functions: api.functions.filter((func) =>
+        func.toLowerCase().includes(query.toLowerCase())
+      ),
+    }));
 
   useEffect(() => {
     // const options: string[] = Object.keys(utils).map((funcName) => {
@@ -67,13 +91,14 @@ export default function Header() {
 
           <div className="flex items-center lg:order-2">
             <div className=" w-72">
-              <Combobox value={selected} onChange={setSelected}>
+              <Combobox>
                 <div className="relative mt-1">
                   {/* input */}
                   <div className="relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus-visible:ring-offset-teal-300 sm:text-sm">
                     <Combobox.Input
+                      placeholder="Search API functions"
                       className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-white focus-within:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-offset-white focus:ring-white"
-                      displayValue={(person: peopleInterface) => person.name}
+                      value={query}
                       onChange={(event) => setQuery(event.target.value)}
                       onClick={() => inputRef.current?.click()}
                     />
@@ -97,36 +122,36 @@ export default function Header() {
                     leaveTo="opacity-0"
                   >
                     <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                      {filteredPeople.length === 0 && query !== "" ? (
+                      {filteredApiList.length === 0 && query !== "" ? (
                         <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                           API not found
                         </div>
                       ) : (
-                        filteredPeople.map((person) => (
-                          <Combobox.Option
-                            key={person.id}
-                            className={({ active }) =>
-                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                active
-                                  ? "bg-gray-900 text-white"
-                                  : "text-gray-900"
-                              }`
-                            }
-                            value={person}
-                          >
-                            {({ selected }) => (
-                              <>
-                                <span
-                                  className={`block truncate ${
-                                    selected ? "font-medium" : "font-normal"
-                                  }`}
-                                >
-                                  {person.name}
-                                </span>
-                              </>
-                            )}
-                          </Combobox.Option>
-                        ))
+                        filteredApiList.map((api) => {
+                          return (
+                            <div key={api?.name} className="mb-3">
+                              <div className="relative cursor-default select-none py-2 pl-2 pr-2 font-bold text-white bg-black">
+                                {api?.name}
+                              </div>
+
+                              {api?.functions.map((func) => {
+                                return (
+                                  <div
+                                    onClick={() => {}}
+                                    key={`${api?.name}-${func}`}
+                                    className={`relative cursor-pointer select-none py-[2px] pl-6 pr-6 text-gray-900`}
+                                  >
+                                    <>
+                                      <span className={`block truncate`}>
+                                        {func}
+                                      </span>
+                                    </>
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          );
+                        })
                       )}
                     </Combobox.Options>
                   </Transition>
